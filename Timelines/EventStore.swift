449c5860115a726d064825242c4c5750d428a099
Daniel Kwolek <Daniel@Daniels-MBP.lan>
2016-10-13 11:10:00 -0400

    added basic cell classes and eventStore file

---
+++ Timelines/EventStore.swift
@@ -0,0 +1,13 @@
+//
+//  EventStore.swift
+//  Timelines
+//
+//  Created by Daniel Kwolek on 10/13/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+import Foundation
+
+class EventStore {
+
+}
---
+++ Timelines/EventsViewController.swift
@@ -0,0 +1,23 @@
+////
+////  EventsViewController.swift
+////  Timelines
+////
+////  Created by Daniel Kwolek on 10/13/16.
+////  Copyright © 2016 Arcore. All rights reserved.
+////
+//
+//import UIKit
+//
+//
+//class EventsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    }
+//    
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        
+//        
+//    }
+//    
+//    
+//}
