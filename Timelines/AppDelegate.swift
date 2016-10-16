645d6277dd12e74425d25a2d53c9eca1bd62c858
Princess Sampson <ps@kingonomics.com>
2016-10-16 13:20:30 -0400

    Will refactor registration VC to break out checking given info into own method, update constraints to ensure password is at least 6 characters, and refactor username character set to use provided alphanumeric set, along with spaces, underscores, and hyphens

--- Timelines/AppDelegate.swift
+++ Timelines/AppDelegate.swift
@@ -16,10 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        //        API.register(body: RegisterRequest(email: "sampson", username: "sampson", password: "sampson")) {
-        //            authResponse in
-        //        }
-        
+//        API.register(body: RegisterRequest(email: "sampson", username: "sampson", password: "sampson")) {
+//            authResponse in
+//        }
+//        
 //        API.login(body: LoginRequest(username: "sampson", password : "sampson")) { authResponse in
 //            UserStore.mainUser = authResponse.user!
 //        }
