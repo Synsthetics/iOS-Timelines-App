f2a13573d48ba3bd2d8da5a8d294e879fc6cd9c9
Daniel Kwolek <Daniel@Daniels-MBP-2.lan>
2016-10-19 16:06:57 -0400

    Have a working version again

--- Timelines/AppDelegate.swift
+++ Timelines/AppDelegate.swift
@@ -15,30 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
         
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        let appTabBarController = storyboard.instantiateViewController(withIdentifier: "AppTabBarController") as! UITabBarController
-        let appNavigationController = UINavigationController(rootViewController: appTabBarController)
         
-        window?.rootViewController = appNavigationController
-        
-        let authNavController = storyboard.instantiateViewController(withIdentifier: "AuthNavController") as! UINavigationController
-        let loginViewController = authNavController.topViewController as! LoginViewController
-        
-        loginViewController.loginCompletion = { user in
-            UserStore.mainUser = user
-            let pendingRequestsViewController = appTabBarController.viewControllers?[2] as! PendingRequestsViewController
-            pendingRequestsViewController.pollForContacts()
-            loginViewController.dismiss(animated: true, completion: nil)
-        }
-       
-        appNavigationController.present(authNavController, animated: false, completion: nil)
-        
+        let appVC = UINavigationController.init(rootViewController: (storyboard.instantiateViewController(withIdentifier:"AppTabBarController")))
+        window?.rootViewController = appVC
         window?.makeKeyAndVisible()
-        
         return true
     }
-    
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
